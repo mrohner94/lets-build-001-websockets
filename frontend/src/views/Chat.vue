16b3ade8d26942a9aa4d2a640bcd510cf85e24a2
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-06-17 19:23:38 -0400

    Update Chat.vue

--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -6,8 +6,17 @@
   <ul id="messages">
     <li v-for="message in messages">{{ message }}</li>
   </ul>
-  <v-form id="form" @submit.prevent="">
-    <v-text-field v-model="inputMessage" />
+  <v-form
+    id="form"
+    @submit.prevent=""
+    class="d-flex align-center justify-space-between"
+  >
+    <input
+      v-model="inputMessage"
+      type="text"
+      class="bg-white rounded w-100 h-100 mr-4"
+      @keydown.enter="onClickSend"
+    />
     <v-btn @click="onClickSend">Send</v-btn>
   </v-form>
 </template>
@@ -19,5 +28,6 @@ const inputMessage = ref("");
 
 const onClickSend = () => {
   console.log("send");
+  inputMessage.value = "";
 };
 </script>
