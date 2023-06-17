3642805ef00f35404f9df4e478afda0de28dac01
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-06-17 18:48:36 -0400

    frontend init

---
+++ frontend/src/views/Chat.vue
@@ -0,0 +1,23 @@
+<style scoped>
+@import "../assets/chat.css";
+</style>
+
+<template>
+  <ul id="messages">
+    <li v-for="message in messages">{{ message }}</li>
+  </ul>
+  <v-form id="form" @submit.prevent="">
+    <v-text-field v-model="inputMessage" />
+    <v-btn @click="onClickSend">Send</v-btn>
+  </v-form>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+const messages = ref<string[]>([]);
+
+const inputMessage = ref("");
+
+const onClickSend = () => {
+  console.log("send");
+};
+</script>
