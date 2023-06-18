9ddd4e8ac1e8d44b6d0559719918b7cf4391f2e3
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-06-17 20:29:47 -0400

    cleanup plugins

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,19 +1,10 @@
-import "./assets/main.css";
-
+import "@/assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
-
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+import { registerPlugins } from "@/plugins";
 
 const app = createApp(App);
 
-app.use(router).use(vuetify).mount("#app");
+registerPlugins(app);
+
+app.mount("#app");
