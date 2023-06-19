b5678090969a120a14870ac757d7421e16e6f431
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-06-18 21:34:33 -0400

    dont really need any of this (could work nanobuffer into it if i want)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,8 @@
-import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import { AllMessage } from "./types";
 
-const app = express();
-const server = http.createServer(app);
+const server = http.createServer();
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:1234",
